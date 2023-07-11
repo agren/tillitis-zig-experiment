8ea47d7e3126e15bc29b1ffc9ce4d281af42a6dd
Mikael Ågren <agrendev@gmail.com>
2023-07-11 21:25:30 +0200

    Declutter

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,38 @@
 const std = @import("std");
 const mmio = @import("tk1_mmio.zig");
 
-pub export fn main() void {
-    mmio.qemu.puts("Running on: '");
-    mmio.qemu.puts(&mmio.tk1.name());
-    mmio.qemu.puts("', version: 0x");
-    mmio.qemu.puthexu32(mmio.tk1.VERSION);
-    mmio.qemu.puts(".\n");
+const tk1 = mmio.tk1;
+const trng = mmio.trng;
+const timer = mmio.timer;
+const touch = mmio.touch;
+const qemu = mmio.qemu;
 
-    mmio.timer.CTRL.fields.STOP = true;
-    mmio.timer.PRESCALER = 18000000; // 18000000: 1 sec/tick
+pub export fn main() void {
+    qemu.puts("Running on: '");
+    qemu.puts(&tk1.name());
+    qemu.puts("', version: 0x");
+    qemu.puthexu32(tk1.VERSION);
+    qemu.puts(".\n");
+
+    timer.CTRL.fields.STOP = true;
+    timer.PRESCALER = 18000000; // 18000000: 1 sec/tick
 
     while (true) {
-        if (!mmio.timer.STATUS.fields.RUNNING) {
-            mmio.timer.TIMER = 1;
-            mmio.timer.CTRL.fields.START = true;
+        if (!timer.STATUS.fields.RUNNING) {
+            timer.TIMER = 1;
+            timer.CTRL.fields.START = true;
 
-            mmio.qemu.puts("Hello world!\n");
+            qemu.puts("Hello world!\n");
 
-            while (!mmio.trng.STATUS.fields.READY) {}
-            mmio.qemu.puts("Entropy is: ");
-            mmio.qemu.puthexu32(mmio.trng.ENTROPY);
-            mmio.qemu.puts(".\n\n");
+            while (!trng.STATUS.fields.READY) {}
+            qemu.puts("Entropy is: ");
+            qemu.puthexu32(trng.ENTROPY);
+            qemu.puts(".\n\n");
         }
 
-        if (mmio.touch.STATUS.fields.EVENT) {
-            mmio.qemu.puts("Touch sensor touched.\n\n");
-            mmio.touch.STATUS.word = 0;
+        if (touch.STATUS.fields.EVENT) {
+            qemu.puts("Touch sensor touched.\n\n");
+            touch.STATUS.word = 0;
         }
     }
 }
